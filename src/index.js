19c2e3917e0d709de453a80d38b0a265dc21675c
Wu Lei <znss1989@gmail.com>
2018-09-26 18:39:44 +0800

    clean up

--- src/index.js
+++ src/index.js
@@ -8,33 +8,3 @@ render(
   <Root />,
   document.getElementById('root')
 )
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
-
-// import ReduxThunk from 'redux-thunk'
-// import { createLogger } from 'redux-logger'
-// import { createStore, applyMiddleware } from 'redux'
-// import { selectSubreddit, fetchPostsIfNeeded } from './actions'
-// import rootReducer from './reducers'
-
-// const loggerMiddleware = createLogger()
-
-// const store = createStore(
-//   rootReducer,
-//   applyMiddleware(
-//     ReduxThunk, // lets us dispatch() functions
-//     loggerMiddleware // neat middleware that logs actions
-//   )
-// )
-
-// store.dispatch(selectSubreddit('reactjs'))
-// store
-//   .dispatch(fetchPostsIfNeeded('reactjs'))
-//   .then(() => console.log(store.getState()))
